06ef16c707101463c887a2e60b61dfbc8e07814d
gowriprasath01 <gowriprasath.18cs@kct.ac.in>
2021-11-17 10:35:07 +0530

    Updated controller and jsp

--- gitDemoapp1/src/main/java/com/ratan/controllers/MyController.java
+++ gitDemoapp1/src/main/java/com/ratan/controllers/MyController.java
@@ -1,8 +1,12 @@
 package com.ratan.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MyController {
 	
-	
+	@RequestMapping("/")
 	public String launchHome() {
 		return "home";
 	}
