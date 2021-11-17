6334e0d0aa43b330835730039c6349e7aba27b68
Ratan <ratan@gmail.com>
2021-11-17 10:19:00 +0530

    first commit with initial setup

---
+++ gitDemoapp1/src/main/java/com/ratan/GitDemoapp1Application.java
@@ -0,0 +1,13 @@
+package com.ratan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoapp1Application.class, args);
+	}
+
+}
---
+++ gitDemoapp1/src/main/java/com/ratan/controllers/MyController.java
@@ -0,0 +1,10 @@
+package com.ratan.controllers;
+
+public class MyController {
+	
+	
+	public String launchHome() {
+		return "home";
+	}
+
+}
